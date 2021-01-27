f73089d7e19f028ade6a6352408a69fa1438f8fd
gliry <49316522+gliry@users.noreply.github.com>
2021-01-27 09:48:11 +0300

    Added FPS and main code

--- Task_1.py
+++ Task_1.py
@@ -1,23 +1,17 @@
 import pygame
 from pygame.draw import *
 
-# После импорта библиотеки, необходимо её инициализировать:
 pygame.init()
 
-# И создать окно:
-screen = pygame.display.set_mode((300, 200))
+FPS = 30
+screen = pygame.display.set_mode((400, 400))
 
-# здесь будут рисоваться фигуры
-# ...
 
-# после чего, чтобы они отобразились на экране, экран нужно обновить:
-pygame.display.update()
-# Эту же команду нужно будет повторять, если на экране происходят изменения.
 
-# Наконец, нужно создать основной цикл, в котором будут отслеживаться
-# происходящие события.
-# Пока единственное событие, которое нас интересует - выход из программы.
-while True:
+while not finished:
+    clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
+            finished = True
+
+pygame.quit()
